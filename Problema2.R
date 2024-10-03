e04ca05a4e56eed372805cf7d59348a100bcde7d
juliamoralees <julia.morales@estudiantat.upc.edu>
2024-10-03 13:28:36 +0200

    Update Problema2.R

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,40 @@
 #Codigo para problema 2
+mis_dades <- iris
+mis_dades
+x <- mis_dades$Petal.Length
+x
+y <- mis_dades$Sepal.Length
+y
+##recta de regressió
+plot(x,y)
+
+x_bar<- mean(x)
+x_bar
+
+y_bar<- mean(y)
+y_bar
+
+m<-sum((x-x_bar)*(y-y_bar))/sum((x-x_bar)^2)
+m
+
+b<-y_bar-m*x_bar
+b
+
+##PREDICCION:
+m*1.5+b
+
+##PREDICCION SOBRE LAS OBSERVACIONES
+y_pred <- m*x+b
+
+plot(x,y)
+lines(x, y_pred)
+
+R_sq <- sum((y_pred-y_bar)^2)/sum((y-y_bar)^2)
+R_sq
+
+####utilitzant les funcions de R ~-> altgr 4 i espai
+
+lm(y~x)
+mod<-lm(y~x)
+mod
+summary(mod)
